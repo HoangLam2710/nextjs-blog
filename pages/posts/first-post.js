33675dd98e86c65a1e1af64219d5853ab124dc7a
Hoang Lam <hoanglam271096@gmail.com>
2021-10-26 20:26:06 +0700

    First Commit NextJs

---
+++ pages/posts/first-post.js
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import Image from "next/image";
+import Head from "next/head";
+import Layout from "../../components/layout";
+
+export default function FirstPost() {
+    return (
+        <Layout>
+            <Head>
+                <title>First Post</title>
+            </Head>
+            <h1>First Post</h1>
+            <h2>
+                <Link href="/">
+                    <a>Back to home</a>
+                </Link>
+            </h2>
+            {/* <img src="/images/profile.jpeg" alt="Your Name" /> */}
+            <Image
+                src="/images/profile.jpeg"
+                height={144}
+                width={144}
+                alt="Your name"
+            />
+        </Layout>
+    );
+}
